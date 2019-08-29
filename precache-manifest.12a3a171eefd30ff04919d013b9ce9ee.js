64b67271cc17f2a0beecf52daa31e45b23fcb35a
Ivan Rivero Alonso <riverinyo@gmail.com>
2019-08-29 00:23:37 -0400

    Updates

---
+++ precache-manifest.12a3a171eefd30ff04919d013b9ce9ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61aec2e322b8d37bcf1e8fdae367eb79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95255d62afc914352503",
+    "url": "/static/css/main.79b8db5c.chunk.css"
+  },
+  {
+    "revision": "b859dd9ac5a2eae6489d",
+    "url": "/static/js/2.d84fa3bc.chunk.js"
+  },
+  {
+    "revision": "95255d62afc914352503",
+    "url": "/static/js/main.6246fad0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6f29b2e520478440c97b0003a2e297ce",
+    "url": "/static/media/me.6f29b2e5.jpg"
+  }
+]);
